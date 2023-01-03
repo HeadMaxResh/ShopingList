5c0b3d66eb4e95a2063eb2c27de9dedab6cfd049
HeadMaxResh <95633686+HeadMaxResh@users.noreply.github.com>
2023-01-03 21:23:46 +0400

    Temporary realise MainViewModel with class from data layer and init block for test in ShopListRepositoryImpl

--- app/src/main/java/com/maxresh/shopinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/maxresh/shopinglist/presentation/MainViewModel.kt
@@ -13,13 +13,24 @@ class MainViewModel: ViewModel() {
     private val repository = ShopListRepositoryImpl
 
     private val getShopListUseCase = GetShopListUseCase(repository)
-    private val deleteShopListUseCase = DeleteShopItemUseCase(repository)
-    private val editShopListUseCase = EditShopItemUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
 
     val shopList = MutableLiveData<List<ShopItem>>()
 
     fun getShopList() {
         val list = getShopListUseCase.getShopList()
         shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeIsActiveState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(isActive = !shopItem.isActive)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
     }
 }
