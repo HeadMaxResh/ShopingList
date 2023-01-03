9b5ac3c2d0785c3d4558f8073778b3914b2cb828
HeadMaxResh <95633686+HeadMaxResh@users.noreply.github.com>
2023-01-03 15:40:34 +0400

    Change inputs useCase add, delete, edit and in repository

---
+++ app/src/main/java/com/maxresh/shopinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.maxresh.shopinglist.data
+
+import com.maxresh.shopinglist.domain.ShopItem
+import com.maxresh.shopinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl: ShopListRepository {
+    override fun addShopItem(ShopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteShopItem(ShopItemId: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editShopItem(ShopItemId: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopItem(ShopItemId: Int): ShopItem {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/maxresh/shopinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/maxresh/shopinglist/domain/ShopListRepository.kt
@@ -2,8 +2,8 @@ package com.maxresh.shopinglist.domain
 
 interface ShopListRepository {
     fun addShopItem(ShopItem: ShopItem)
-    fun deleteShopItem(ShopItemId: ShopItem)
-    fun editShopItem(ShopItemId: ShopItem)
-    fun getShopItem(ShopItemId: Int): ShopItem
+    fun deleteShopItem(ShopItem: ShopItem)
+    fun editShopItem(ShopItem: ShopItem)
+    fun getShopItem(ShopItem: Int): ShopItem
     fun getShopList(): List<ShopItem>
 }
