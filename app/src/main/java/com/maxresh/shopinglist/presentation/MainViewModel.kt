57754b8a9ec402ffd37b2173707ea0caabbf8de8
HeadMaxResh <95633686+HeadMaxResh@users.noreply.github.com>
2023-01-04 14:51:13 +0400

    Changed realise useCases to update in ShopListRepositoryImpl and realise GetShopListUseCase to return LiveData

--- app/src/main/java/com/maxresh/shopinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/maxresh/shopinglist/presentation/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.maxresh.shopinglist.presentation
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.maxresh.shopinglist.data.ShopListRepositoryImpl
 import com.maxresh.shopinglist.domain.DeleteShopItemUseCase
@@ -16,21 +15,14 @@ class MainViewModel: ViewModel() {
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
-    val shopList = MutableLiveData<List<ShopItem>>()
-
-    fun getShopList() {
-        val list = getShopListUseCase.getShopList()
-        shopList.value = list
-    }
+    val shopList = getShopListUseCase.getShopList()
 
     fun deleteShopItem(shopItem: ShopItem) {
         deleteShopItemUseCase.deleteShopItem(shopItem)
-        getShopList()
     }
 
     fun changeIsActiveState(shopItem: ShopItem) {
         val newItem = shopItem.copy(isActive = !shopItem.isActive)
         editShopItemUseCase.editShopItem(newItem)
-        getShopList()
     }
 }
