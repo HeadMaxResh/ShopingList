6bf4f9366b1a768d8e0f235b5f152afcc93374dc
HeadMaxResh <95633686+HeadMaxResh@users.noreply.github.com>
2023-01-06 06:59:12 +0400

    Remove realize with LinearLayout

--- app/src/main/java/com/maxresh/shopinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/maxresh/shopinglist/presentation/MainActivity.kt
@@ -12,35 +12,13 @@ import com.maxresh.shopinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
-    private lateinit var llShopList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        llShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            showList(it)
-        }
-    }
-    private fun showList(list: List<ShopItem>) {
-        llShopList.removeAllViews()
-        for (shopItem in list) {
-            val layoutId = if (shopItem.isActive) {
-                R.layout.item_shop_active
-            } else {
-                R.layout.item_shop_inactive
-            }
-            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
-            val tvName = view.findViewById<TextView>(R.id.tv_name)
-            val tvCount = view.findViewById<TextView>(R.id.tv_count)
-            tvName.text = shopItem.name
-            tvCount.text = shopItem.count.toString()
-            view.setOnLongClickListener {
-                viewModel.changeIsActiveState(shopItem)
-                true
-            }
-            llShopList.addView(view)
+
         }
     }
 }
