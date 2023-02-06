fed40235d4352dca0d2cda18514fa9ab48b7ff4f
HeadMaxResh <95633686+HeadMaxResh@users.noreply.github.com>
2023-02-06 14:51:09 +0400

    Added method onViewRecycled with default values Added companion object with index to active and inactive and max pool size, Added method getItemViewType and added in onCreateViewHolder terms of use for layouts

--- app/src/main/java/com/maxresh/shopinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/maxresh/shopinglist/presentation/ShopListAdapter.kt
@@ -1,26 +1,31 @@
 package com.maxresh.shopinglist.presentation
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.maxresh.shopinglist.R
 import com.maxresh.shopinglist.domain.ShopItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
+    var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_shop_active,
-            parent,
-            false
-        )
+        Log.d("ShopListAdapter", "onCreateViewHolder, count: ${++count}")
+        val layout = when (viewType) {
+            VIEW_TYPE_ACTIVE -> R.layout.item_shop_active
+            VIEW_TYPE_INACTIVE -> R.layout.item_shop_inactive
+            else -> throw RuntimeException("Unknown view type: $viewType")
+        }
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent,false)
         return ShopItemViewHolder(view)
     }
 
@@ -34,12 +39,41 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
         }
     }
 
+    override fun onViewRecycled(viewHolder: ShopItemViewHolder) {
+        super.onViewRecycled(viewHolder)
+        viewHolder.tvName.text = ""
+        viewHolder.tvCount.text = ""
+        viewHolder.tvName.setTextColor(
+            ContextCompat.getColor(
+                viewHolder.view.context,
+                android.R.color.white
+            )
+        )
+    }
+
     override fun getItemCount(): Int {
         return shopList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val item = shopList[position]
+        return if (item.isActive) {
+            VIEW_TYPE_ACTIVE
+        } else {
+            VIEW_TYPE_INACTIVE
+        }
     }
 
     class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val tvName: TextView = view.findViewById(R.id.tv_name)
         val tvCount: TextView = view.findViewById(R.id.tv_count)
+    }
+
+    companion object {
+
+        const val VIEW_TYPE_ACTIVE = 100
+        const val VIEW_TYPE_INACTIVE = 101
+
+        const val MAX_POOL_SIZE = 30
     }
 }
