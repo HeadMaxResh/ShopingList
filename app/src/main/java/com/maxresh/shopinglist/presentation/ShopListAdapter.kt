0013dddb0ed01e3dd679791f85ae614462cc683f
HeadMaxResh <95633686+HeadMaxResh@users.noreply.github.com>
2023-03-06 10:26:22 +0400

    Rescheduled class ShopItemViewHolder from ShopListAdapter

--- app/src/main/java/com/maxresh/shopinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/maxresh/shopinglist/presentation/ShopListAdapter.kt
@@ -1,26 +1,17 @@
 package com.maxresh.shopinglist.presentation
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.maxresh.shopinglist.R
 import com.maxresh.shopinglist.domain.ShopItem
-import androidx.recyclerview.widget.ListAdapter
 
-class ShopListAdapter : ListAdapter<ShopItem, ShopListAdapter.ShopItemViewHolder>(ShopItemDiffCallback()) {
-
-    var count = 0
+class ShopListAdapter : ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCallback()) {
 
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        Log.d("ShopListAdapter", "onCreateViewHolder, count: ${++count}")
         val layout = when (viewType) {
             VIEW_TYPE_ACTIVE -> R.layout.item_shop_active
             VIEW_TYPE_INACTIVE -> R.layout.item_shop_inactive
@@ -47,18 +38,6 @@ class ShopListAdapter : ListAdapter<ShopItem, ShopListAdapter.ShopItemViewHolder
 
     }
 
-    override fun onViewRecycled(viewHolder: ShopItemViewHolder) {
-        super.onViewRecycled(viewHolder)
-        viewHolder.tvName.text = ""
-        viewHolder.tvCount.text = ""
-        viewHolder.tvName.setTextColor(
-            ContextCompat.getColor(
-                viewHolder.view.context,
-                android.R.color.white
-            )
-        )
-    }
-
     override fun getItemViewType(position: Int): Int {
         val item = getItem(position)
         return if (item.isActive) {
@@ -66,11 +45,6 @@ class ShopListAdapter : ListAdapter<ShopItem, ShopListAdapter.ShopItemViewHolder
         } else {
             VIEW_TYPE_INACTIVE
         }
-    }
-
-    class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val tvName: TextView = view.findViewById(R.id.tv_name)
-        val tvCount: TextView = view.findViewById(R.id.tv_count)
     }
 
     companion object {
